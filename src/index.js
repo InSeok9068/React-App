b5458b5f3eef4dd547ca44e8d15fdcdbf9ba7af2
Lee In Seok <dlstjr9068@naver.com>
2019-04-18 23:19:41 +0900

    04/18

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 var element = React.createElement('h1', {className: 'greeting'}, 'Hello, world!')
