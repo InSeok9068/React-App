d04972ec8a777e004a7338682c03f7c52045ec9c
Lee In Seok <dlstjr9068@naver.com>
2019-04-20 00:44:33 +0900

    4월20일

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,22 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from 'react';
 import './Movie.css';
 
 class Movie extends Component{
     render(){
         return(
+            <div>
+                <MoviePoster/>
             <h1>hello this is moive</h1>
+            </div>
+        )
+    }
+}
+
+class MoviePoster extends Component{
+    render(){
+        return(
+            <img src="https://i.ytimg.com/vi/HoqmtbtL3uo/maxresdefault.jpg"/>
         )
     }
 }
